b9a22d9696f2b9e17ba10b47fdb2e2014c0dcead
231101 <231101@CPD02-M06>
2025-03-24 12:25:04 -0300

    24/03- última atualização 12:24

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MauiAppMinhasCompras.Helpers;
+using System.Globalization;
 using MauiAppMinhasCompras.Views;
 
 namespace MauiAppMinhasCompras
@@ -28,6 +29,9 @@ namespace MauiAppMinhasCompras
         {
             InitializeComponent();
 
+            //  Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
+
+            //MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
